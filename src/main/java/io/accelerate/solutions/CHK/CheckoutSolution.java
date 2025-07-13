7f98fd9dc31531ded30e8746ccf4c7b47730dcc6
sbx_user1051 <sbx_user1051@169.254.3.105>
2025-07-13 13:57:42 +0000

    Sun Jul 13 13:57:42 UTC 2025

--- src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
+++ src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
@@ -1,12 +1,22 @@
 package io.accelerate.solutions.CHK;
 
+import java.util.HashMap;
+
 import io.accelerate.runner.SolutionNotImplementedException;
 
 public class CheckoutSolution {
     public Integer checkout(String skus) {
-        System.out.println(skus); 
-        return -1;
-        //I appreciate this adds time, but I want to see what they look like
+        HashMap<Character, Integer> prices = new HashMap<Character, Integer>();
+        prices.put('A', 50);
+        prices.put('B', 30);
+        prices.put('C', 20);
+        prices.put('D', 15);
+        int total = -1;
+        for(Character c : skus.toCharArray()) {
+            total += prices.get(c);
+        }
+        return total;
     }
 }
+
 
