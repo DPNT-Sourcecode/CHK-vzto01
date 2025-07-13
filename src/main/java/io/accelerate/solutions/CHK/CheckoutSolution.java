24ba6ca330611f3099e5bf150ddd015f4f3d6736
sbx_user1051 <sbx_user1051@169.254.3.105>
2025-07-13 14:04:33 +0000

    Sun Jul 13 14:04:33 UTC 2025

--- src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
+++ src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
@@ -12,11 +12,23 @@ public class CheckoutSolution {
         prices.put('C', 20);
         prices.put('D', 15);
         int total = -1;
+        int aCount = 0;
+        int bCount = 0;
         for(Character c : skus.toCharArray()) {
-            total += prices.get(c);
+            if (c == 'A'){
+                aCount ++;
+            }else if (c == 'B') {
+                bCount ++;
+            } 
+            else {
+                total += prices.get(c);
+            }
         }
+        total += ((130 * Math.floorDiv(aCount, 3)) + ((aCount % 3) * 50)); //work out how many times we have 3 of A, times that by 130, and then add on any remaining A's
+        total += ((45 * Math.floorDiv(bCount, 2)) + ((aCount % 3) * 30));
         return total;
     }
 }
+
 
 
