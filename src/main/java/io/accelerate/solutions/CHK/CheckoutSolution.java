a267214624c1d09651a44b4f6280f6151fb4bed8
sbx_user1051 <sbx_user1051@169.254.3.105>
2025-07-13 15:49:25 +0000

    Sun Jul 13 15:49:25 UTC 2025

--- src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
+++ src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
@@ -29,12 +29,33 @@ public class CheckoutSolution {
 
     public Integer checkout(String skus) {
         HashMap<Character, Integer> prices = new HashMap<Character, Integer>();
+        //There's a better way of doing this, I'd like to be given a csv and then just read that in but it'd take just as long to write the csv
         prices.put('A', 50);
         prices.put('B', 30);
         prices.put('C', 20);
         prices.put('D', 15);
         prices.put('E', 40);
         prices.put('F', 10);
+        prices.put('G', 20);
+        prices.put('H', 10);
+        prices.put('I', 35);
+        prices.put('J', 60);
+        prices.put('K', 80);
+        prices.put('L', 90);
+        prices.put('M', 15);
+        prices.put('N', 40);
+        prices.put('O', 10);
+        prices.put('P', 50);
+        prices.put('Q', 30);
+        prices.put('R', 50);
+        prices.put('S', 30);
+        prices.put('T', 20);
+        prices.put('U', 40);
+        prices.put('V', 50);
+        prices.put('W', 20);
+        prices.put('X', 90);
+        prices.put('Y', 10);
+        prices.put('Z', 50);
 
         HashMap<Character, Integer> items = new HashMap<Character, Integer>();
         if (Pattern.compile("[^A-Z]+").matcher(skus).find() == true) {
@@ -57,14 +78,13 @@ public class CheckoutSolution {
 
         total += xForY('A', items, 5, 200);
         total += xForY('A', items, 3, 130);
-        total += xForY('A', items, 2, 45);
+        total += xForY('B', items, 2, 45);
         total += xForY('H', items, 5, 45);
         total += xForY('H', items, 10, 80);
         total += xForY('K', items, 2, 150);
         total += xForY('P', items, 5, 200);
-        total += xForY('Q', items, 35, 80);
+        total += xForY('Q', items, 3, 80);
         total += xForY('V', items, 2, 90);
-        total += xForY('Q', items, 35, 80);
         total += xForY('V', items, 3, 130);
 
         getOneFree('E', items, 2, 'B');
@@ -73,18 +93,6 @@ public class CheckoutSolution {
         getOneFree('R', items, 3, 'Q');
         getOneFree('U', items, 3, 'U');
 
-        // if E/2 >=1, add 1-B-free deal and remove a B from list
-        if (items.get('E') != null && items.get('B') != null && items.get('E') > 0) {
-            int dealE = Math.floorDiv(items.get('E'), 2);
-            items.put('B', (Math.max((items.get('B') - dealE), 0)));
-        }
-
-        // if F/3 >=1, add 3-4-2 deal and remove an F from list
-        if (items.get('F') != null && items.get('F') > 0) {
-            int dealF = Math.floorDiv(items.get('F'), 3);
-            items.put('F', (items.get('F') - dealF));
-        }
-
         // tally up remaining items
         for (Character k : items.keySet()) {
             total += (items.get(k) * prices.getOrDefault(k, 0));
@@ -92,6 +100,7 @@ public class CheckoutSolution {
         return total;
     }
 }
+
 
 
 
